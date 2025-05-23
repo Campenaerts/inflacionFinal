32e0519b31b09ef633c59bb0710bbef03e6e342b
Campenaerts <boobamzz@hotmail.com>
2025-05-23 17:19:10 -0500

    Add files via upload

--- cargar_postgres.py
+++ cargar_postgres.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 
 df = pd.read_excel("inflacion_procesada.xlsx")
 
-usuario = "postgres"
-contraseña = "luukde1ong"  
-host = "localhost"
+usuario = "dataviz_db_a2qu_user"
+contraseña = "aZ1oCNwaqUh2SruEvnFrNSO1Tp2Hk6ti"  
+host = "dpg-d0oe98muk2gs73bp2g3g-a.oregon-postgres.render.com"
 puerto = "5432"
-base_datos = "dataviz_db"
+base_datos = "dataviz_db_a2qu"
 
 engine = create_engine(f"postgresql+psycopg2://{usuario}:{contraseña}@{host}:{puerto}/{base_datos}")
 
